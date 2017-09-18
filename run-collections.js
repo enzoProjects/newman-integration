1da6520fe9fcd4e75d857bcee463ad82ac3aba50
Enzo Bonggio <springfield@Enzos-MBP.fibertel.com.ar>
2017-09-18 00:49:21 -0300

    refactor let instead of var

--- run-collections.js
+++ run-collections.js
@@ -3,37 +3,35 @@
  * @fileOverview This sample code illustrates how one can read all collection files within a directory and run them
  * in parallel.
  */
-var newman = require('newman')
-    fs = require('fs');
+let newman = require('newman');
 
-var collectionsDir = './collections';
-var environmentDir = './enviorments';
-var ambiente = 'dev';
+let collectionsDir = './collections';
+let environmentDir = './enviorments';
+let ambiente = 'dev';
 
 
-var result = newman
+newman
     .run({
         // corremos collection que tiene las globalVariables
         collection: require(`${__dirname}/${collectionsDir}/1.SettingGlobalVariables.postman_collection.json`),
         environment: require(`${environmentDir}/${ambiente}.postman_environment.json`),
-})
+    })
     .on('done', function (err, summary) {
         if (err || summary.error) {
             console.error('collection run encountered an error.');
         }
         else {
             newman.run({
-                    // we load collection using require. for better validation and handling
-                    // JSON.parse could be used
-                    collection: require(`${__dirname}/${collectionsDir}/2.MS-1.postman_collection.json`),
-                    environment: summary.environment,
-                    globals: summary.globals,
-                    reporters: ['html'],
+                // aca podriamos tener un foreach pero habria que especificar un html
+                collection: require(`${__dirname}/${collectionsDir}/2.MS-1.postman_collection.json`),
+                environment: summary.environment,
+                globals: summary.globals,
+                reporters: ['html'],
             }).on('done', function (err, summary) {
                 if (err || summary.error) {
                     console.error('collection run encountered an error.');
                 } else console.info('done');
             });
         }
-});
+    });
 

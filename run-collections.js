821f010e255b1954382eee3cf935e7eb8e170df2
Enzo Bonggio <springfield@Enzos-MBP.fibertel.com.ar>
2017-09-18 00:41:27 -0300

    html report

--- run-collections.js
+++ run-collections.js
@@ -7,28 +7,33 @@ var newman = require('newman')
     fs = require('fs');
 
 var collectionsDir = './collections';
-var enviormentsDir = './enviorments';
+var environmentDir = './enviorments';
 var ambiente = 'dev';
 
 
-var result = newman.run({
-            // we load collection using require. for better validation and handling
-            // JSON.parse could be used
-            collection: require(`${__dirname}/${collectionsDir}/1.SettingGlobalVariables.postman_collection.json`),
-            environment: require(`${enviormentsDir}/${ambiente}.postman_environment.json`),
-            reporters: 'cli'
-        }, function (err, summ) {
-            // finally, when the collection executes, print the status
+var result = newman
+    .run({
+        // corremos collection que tiene las globalVariables
+        collection: require(`${__dirname}/${collectionsDir}/1.SettingGlobalVariables.postman_collection.json`),
+        environment: require(`${environmentDir}/${ambiente}.postman_environment.json`),
+})
+    .on('done', function (err, summary) {
+        if (err || summary.error) {
+            console.error('collection run encountered an error.');
+        }
+        else {
             newman.run({
-                // we load collection using require. for better validation and handling
-                // JSON.parse could be used
-                collection: require(`${__dirname}/${collectionsDir}/2.MS-1.postman_collection.json`),
-                environment: require(`${__dirname}/${enviormentsDir}/${ambiente}.postman_environment.json`),
-                globals: summ.globals,
-                reporters: 'cli'
-            }, function (err, summ) {
-                // finally, when the collection executes, print the status
-    });
+                    // we load collection using require. for better validation and handling
+                    // JSON.parse could be used
+                    collection: require(`${__dirname}/${collectionsDir}/2.MS-1.postman_collection.json`),
+                    environment: summary.environment,
+                    globals: summary.globals,
+                    reporters: ['html'],
+            }).on('done', function (err, summary) {
+                if (err || summary.error) {
+                    console.error('collection run encountered an error.');
+                } else console.info('done');
+            });
+        }
 });
-
 
